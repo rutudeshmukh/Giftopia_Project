546b167c2e1fdfba041beb3a4222627aa9e60ecf
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-06-11 00:15:52 +0530

    Pagination - 111 complete

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -10,6 +10,9 @@ import {Routes,RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 //order of route is important
 const routes: Routes=[
 {path:'products/:id',component: ProductDetailsComponent},
@@ -32,7 +35,8 @@ const routes: Routes=[
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,6 +14,13 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
   searchMode: boolean = false;
+  previousCategoryId: number = 1;
+
+  //new properties for pagination
+  thePageNumber: number =1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
+
 
   constructor(
     private productService: ProductService,
@@ -61,11 +68,36 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    //check if we have a different category than previous
+    //Note: Angular will reuse a component if it is currently being viewed
+
+    //if we have a different category id than previos
+    //the set thePageNumber back to 1
+    if(this.previousCategoryId!= this.currentCategoryId){
+      this.thePageNumber=1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId},thePageNumber=${this.thePageNumber}`);
+    
+    
     //now get the products for the given category Id
-    this.productService
-      .getProductList(this.currentCategoryId)
-      .subscribe((data) => {
-        this.products = data;
-      });
+    
+    this.productService.getProductListPaginate(this.thePageNumber =1,
+                                               this.thePageSize,
+                                               this.currentCategoryId)
+                                               .subscribe(
+                                                data=> {
+                                                    this.products = data._embedded.products;
+                                                    this.thePageNumber =data.page.number + 1; // spring Data REST: Pages are 0 based
+                                                    this.thePageSize = data.page.size;
+                                                    this.theTotalElements = data.page.totalElements;
+                                                });
+    // this.productService
+    //   .getProductList(this.currentCategoryId)
+    //   .subscribe((data) => {
+    //     this.products = data;
+    //   });
   }
 }
