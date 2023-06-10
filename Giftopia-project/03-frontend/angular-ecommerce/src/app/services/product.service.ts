257113f6bff30365c5a56d72e8c850ee817f49a9
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-06-10 18:01:55 +0530

    Product Master-Detail View

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -7,13 +7,21 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
+  
 
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient : HttpClient) { }
+  
+  getProduct(theProductId: number) : Observable<Product>{
+    //need to build URL based on product id
+  const productUrl =`${this.baseUrl}/${theProductId}`;
 
+  return this.httpClient.get<Product>(productUrl);
+  
+  }
   getProductList(theCategoryId: number) : Observable<Product[]>{
 
     //need to build URL based on category id
