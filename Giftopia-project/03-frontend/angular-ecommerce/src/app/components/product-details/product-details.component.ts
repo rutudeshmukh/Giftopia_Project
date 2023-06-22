99bb253b8952cbf7213c3627ecbca19f007c9ad0
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-06-22 22:47:35 +0530

    Shopping cart status component

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,8 +12,10 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductDetailsComponent implements OnInit{
 
-  product!: Product;
+  product: Product = new Product();
+
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute){}
   ngOnInit(): void {
     this.route.paramMap.subscribe(()=>{
@@ -29,6 +33,13 @@ export class ProductDetailsComponent implements OnInit{
         this.product=data;
       }
     )
+  }
+
+  addToCart(){
+
+    console.log(`Adding to cart: ${this.product.name},${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem); 
   }
 
 }
--- Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -26,6 +28,7 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -124,7 +127,9 @@ export class ProductListComponent implements OnInit {
 
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
 
-    //TODO
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
 
   }
 
