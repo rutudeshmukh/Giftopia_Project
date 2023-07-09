b903cfa284759608549f3a842e7573dcd0223f99
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-07-09 13:36:15 +0530

    shopping cart items CRUD

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -71,4 +71,29 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity==0){
+      this.remove(theCartItem);
+    }
+    else{
+      this.computeCartTotals();
+    }
+    
+  }
+  remove(theCartItem: CartItem) {
+    
+    //get index of the item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    //if found, remove the item from the array at the givem index
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex,1);
+
+      this.computeCartTotals();
+    }
+  }
 }
