a24f94461e990a1596ff66237afc3bb78b7008d5
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-07-16 16:45:29 +0530

    till 181

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { GiftopiaFormService } from 'src/app/services/giftopia-form.service';
@@ -9,157 +9,176 @@ import { GiftopiaFormService } from 'src/app/services/giftopia-form.service';
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit{
+export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
 
-  totalPrice:number = 0;
+  totalPrice: number = 0;
   totalQuantity: number = 0;
-
+  
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
 
-  countries: Country[]=[];
+  countries: Country[] = [];
+
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
-
-  constructor(private formBuilder:FormBuilder,
-              private giftopiaFormService: GiftopiaFormService){}
+  
+  
+  constructor(private formBuilder: FormBuilder,
+              private giftopiaFormService: GiftopiaFormService) { }
 
   ngOnInit(): void {
-      
+    
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName:[''],
-        lastName:[''],
-        email:['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName:  new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+                              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:['']
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
       }),
       billingAddress: this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:['']
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
       }),
       creditCard: this.formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
-        expirationMonth:[''],
-        expirationYear:['']
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
 
-    //populate credit card months
-    const startMonth:number = new Date().getMonth()+1;
-    console.log("startMonth:" + startMonth);
+    // populate credit card months
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
 
     this.giftopiaFormService.getCreditCardMonths(startMonth).subscribe(
-      data=>{
-        console.log("Retrieved credit card months:" + JSON.stringify(data));
-        this.creditCardMonths=data;
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
       }
     );
 
-    //populate credit card years
+    // populate credit card years
+
     this.giftopiaFormService.getCreditCardYears().subscribe(
-      data=>{
-        console.log("Retrieved credit card years:" + JSON.stringify(data));
-        this.creditCardYears=data;
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
       }
     );
 
-    //populate countries
+    // populate countries
+
     this.giftopiaFormService.getCountries().subscribe(
-      data=>{
-      console.log("Retrieved countries: " + JSON.stringify(data));
-      this.countries=data;
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
       }
     );
   }
 
-    copyShippingAddressToBillingAddress(event){
-      if(event.target.checked){
-        this.checkoutFormGroup.controls['billingAddress']
-        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
-      
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+            .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+
       // bug fix for states
       this.billingAddressStates = this.shippingAddressStates;
-      }
-      else{
-        this.checkoutFormGroup.controls['billingAddress'].reset();
-        // bud fix for states
-        this.billingAddressStates = [];
-      }
 
     }
-    onSubmit(){
-      console.log("Handling the submit button");
-      console.log(this.checkoutFormGroup.get('customer').value);
-      console.log("The email address is "+ this.checkoutFormGroup.get('customer').value.email);
-      
-      console.log("The shipping address country is "+ this.checkoutFormGroup.get('shippingAddress').value.country.name);
-      console.log("The shipping address state is "+ this.checkoutFormGroup.get('shippingAddress').value.state.name);
-      
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+
+      // bug fix for states
+      this.billingAddressStates = [];
     }
-
-    handleMonthsAndYears(){
-
-      const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
-
-      const currentYear: number = new Date().getFullYear();
-      const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
-
-      //if the current year equals the selected year, then start with the current month
-
-      let startMonth: number;
-
-      if(currentYear === selectedYear){
-        startMonth = new Date().getMonth() + 1;
-      }else{
-        startMonth = 1;
-      }
-
-      this.giftopiaFormService.getCreditCardMonths(startMonth).subscribe(
-        data=>{
-          console.log("Retrieved credit card months: "+ JSON.stringify(data));
-          this.creditCardMonths=data;
-        }
-      )
-    }
-
-    getStates(formGroupName: string){
-
-      const formGroup = this.checkoutFormGroup.get(formGroupName);
-
-      const countryCode = formGroup.value.country.code;
-      const countryName = formGroup.value.country.name;
-
-      console.log(`${formGroupName} country code: ${countryCode}`);
-      console.log(`${formGroupName} country code: ${countryName}`);
     
-      this.giftopiaFormService.getStates(countryCode).subscribe(
-        data=>{
-          if(formGroupName === 'shippingAddress'){
-            this.shippingAddressStates = data;
-          }
-          else{
-            this.billingAddressStates = data;
-          }
+  }
 
-          //select first item by default
-          formGroup.get('state').setValue(data[0]);
-        }
-      );
-    
+  onSubmit() {
+    console.log("Handling the submit button");
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
     }
 
+    console.log(this.checkoutFormGroup.get('customer').value);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+  
+    console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
+  
+  }
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.giftopiaFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.giftopiaFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data; 
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
+  }
 }
