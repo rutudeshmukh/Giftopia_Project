102f2cfdcddbb7484578c8085fa873b115391feb
Rutuja Deshmukh <rutujad3498@gmail.com>
2023-08-04 16:37:27 +0530

    Secure communication with HTTPS : Angular FrontEnd

--- Giftopia-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ Giftopia-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId : '0oaamosrl54GjfdNF5d7',
         issuer: 'https://dev-19750369.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile','email']
     }
 }
